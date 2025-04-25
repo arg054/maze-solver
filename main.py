2d4784d49c3201a80b199cf5d0c9037571356789
arg054 <maxim.golovenkov@gmail.com>
2025-04-25 14:30:09 +0200

    implementing point and line classes, adding draw_line method

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from window import Window
+from window import Window, Point, Line
 
 win = Window(800, 600)
+
+point1 = Point(5, 5)
+point2 = Point(150, 150)
+
+line = Line(point1, point2)
+
+win.draw_line(line, "red")
 win.wait_for_close()
