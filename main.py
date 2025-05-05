393c327647cb71996a1db6ceee8caafeb0f6397b
arg054 <maxim.golovenkov@gmail.com>
2025-05-05 11:20:33 +0200

    implementing draw_move

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from window import Window, Cell
+from window import Window, Cell, Point, Line
 
 win = Window(800, 600)
 
 cell1 = Cell(5, 50, 5, 50, win)
 cell2 = Cell(50, 100, 50, 100, win)
 
+
 cell1.has_left_wall = False
 cell2.has_right_wall = False
 
 cell1.draw("black")
 cell2.draw("black")
+
+cell1.draw_move(cell2)
+
 win.wait_for_close()
--- window.py
+++ window.py
@@ -79,3 +79,16 @@ class Cell:
                 Line(Point(self._x1, self._y2), Point(self._x2, self._y2)),
                 fill_color,
             )
+
+    def draw_move(self, to_cell, undo=False):
+        color = "red"
+        if undo:
+            color = "grey"
+
+        self._win.draw_line(
+            Line(
+                Point((self._x1 + self._x2) / 2, (self._y1 + self._y2) / 2),
+                Point((to_cell._x1 + to_cell._x2) / 2, (to_cell._y1 + to_cell._y2) / 2),
+            ),
+            color,
+        )
