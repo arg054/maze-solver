1a281fba57b281b463a143cf7abc52a9b2f395f7
arg054 <maxim.golovenkov@gmail.com>
2025-05-09 11:55:21 +0200

    implementing _break_entrance_and_exit and its unit tests

--- window.py
+++ window.py
@@ -65,6 +65,7 @@ class Cell:
         self._y1 = y1
         self._y2 = y2
 
+        # TODO redesign this section
         if self._win:
 
             if self.has_left_wall:
@@ -72,20 +73,40 @@ class Cell:
                     Line(Point(self._x1, self._y1), Point(self._x1, self._y2)),
                     "black",
                 )
+            else:
+                self._win.draw_line(
+                    Line(Point(self._x1, self._y1), Point(self._x1, self._y2)),
+                    "white",
+                )
             if self.has_right_wall:
                 self._win.draw_line(
                     Line(Point(self._x2, self._y1), Point(self._x2, self._y2)),
                     "black",
+                )
+            else:
+                self._win.draw_line(
+                    Line(Point(self._x2, self._y1), Point(self._x2, self._y2)),
+                    "white",
                 )
             if self.has_top_wall:
                 self._win.draw_line(
                     Line(Point(self._x1, self._y1), Point(self._x2, self._y1)),
                     "black",
                 )
+            else:
+                self._win.draw_line(
+                    Line(Point(self._x1, self._y1), Point(self._x2, self._y1)),
+                    "white",
+                )
             if self.has_bottom_wall:
                 self._win.draw_line(
                     Line(Point(self._x1, self._y2), Point(self._x2, self._y2)),
                     "black",
+                )
+            else:
+                self._win.draw_line(
+                    Line(Point(self._x1, self._y2), Point(self._x2, self._y2)),
+                    "white",
                 )
 
     def draw_move(self, to_cell, undo=False):
@@ -125,6 +146,8 @@ class Maze:
             for row in range(self.num_rows):
                 self._draw_cell(row, col)
 
+        self._break_entrance_and_exit()
+
     def _draw_cell(self, row, col):
         x1 = self.x1 + (row * self.cell_size_x)
         x2 = x1 + self.cell_size_x
@@ -133,6 +156,16 @@ class Maze:
 
         self._cells[col][row].draw(x1, x2, y1, y2)
         self._animate()
+
+    def _break_entrance_and_exit(self):
+        self._cells[0][0].has_top_wall = False
+        self._draw_cell(0, 0)
+
+        self._cells[-1][-1].has_bottom_wall = False
+        self._draw_cell(
+            self.num_rows - 1,
+            self.num_cols - 1,
+        )
 
     def _animate(self):
         if self._win:
